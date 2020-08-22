cd34afbcd3f756192fbd5d065549c84e8fca7024
Eduardo Borsa <borsa.dado@gmail.com>
2020-08-22 07:49:49 -0300

    End of Chapter 3 of Programmers Passport
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

---
+++ .iex.exs
@@ -0,0 +1,1 @@
+alias SuperDuper.{Core, Server}
--- lib/super_duper/application.ex
+++ lib/super_duper/application.ex
@@ -4,16 +4,20 @@ defmodule SuperDuper.Application do
   @moduledoc false
 
   use Application
+  alias SuperDuper.Server
 
   def start(_type, _args) do
+    IO.puts(">>>> Starting Super-Duper Supervisor <<<<<")
+
     children = [
-      # Starts a worker by calling: SuperDuper.Worker.start_link(arg)
-      # {SuperDuper.Worker, arg}
+      {Server, :superdave},
+      {Server, :superman},
+      {Server, :supermario}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: SuperDuper.Supervisor]
+    opts = [strategy: :rest_for_one, name: SuperDuper.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
